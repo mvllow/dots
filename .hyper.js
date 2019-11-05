d6dee5344ec60babd98fb39edc3c9e59bb6e5f49
mvllow <mvllow@icloud.com>
2019-11-05 00:40:22 -0600

    update colors (thanks to @pacocoursey)

--- .hyper.js
+++ .hyper.js
@@ -1,7 +1,44 @@
 module.exports = {
   config: {
-    fontFamily: "SF Mono, SFMono-Regular, Menlo, Consolas, Courier, monospace",
-    fontSize: 12
+    updateChannel: "canary",
+
+    fontSize: 12,
+    fontFamily: "SF Mono, SFMono-Regular, Menlo, Consolas",
+
+    cursorColor: "#fff",
+    foregroundColor: "#fff",
+    backgroundColor: "#000",
+    selectionColor: "rgba(255, 255, 255, 0.01)",
+    borderColor: "rgba(0, 0, 0, 0)",
+
+    padding: "12px 30px 30px 30px",
+
+    colors: {
+      black: "#000",
+      white: "#fff",
+
+      red: "#f0c098",
+      green: "#4e709d",
+      yellow: "#f0c098",
+      blue: "#ccc",
+      magenta: "#ccc",
+      cyan: "#4e709d",
+
+      lightBlack: "#777",
+      lightWhite: "#900fff",
+
+      lightRed: "#f0c098",
+      lightGreen: "#4e709d",
+      lightYellow: "#ccc",
+      lightBlue: "#f0c098",
+      lightMagenta: "#ccc",
+      lightCyan: "#50e3c2"
+    },
+
+    shell: "",
+    shellArgs: ["--login"],
+
+    bell: false
   },
-  plugins: ["hypercwd", "hyper-website-theme"]
+  plugins: []
 };
