79c3d6de801d5752b07f1f0efe8dd38501a28a74
mvllow <mvllow@icloud.com>
2019-11-12 22:25:16 -0600

    add theme

--- .hyper.js
+++ .hyper.js
@@ -3,7 +3,7 @@ module.exports = {
     updateChannel: "canary",
 
     fontSize: 12,
-    fontFamily: "SF Mono, SFMono-Regular, Menlo, Consolas",
+    fontFamily: "'SF Mono', SFMono-Regular, Menlo, Consolas",
 
     cursorColor: "#fff",
     foregroundColor: "#fff",
@@ -13,32 +13,10 @@ module.exports = {
 
     padding: "12px 30px 30px 30px",
 
-    colors: {
-      black: "#000",
-      white: "#fff",
-
-      red: "#f0c098",
-      green: "#4e709d",
-      yellow: "#f0c098",
-      blue: "#ccc",
-      magenta: "#ccc",
-      cyan: "#4e709d",
-
-      lightBlack: "#777",
-      lightWhite: "#900fff",
-
-      lightRed: "#f0c098",
-      lightGreen: "#4e709d",
-      lightYellow: "#ccc",
-      lightBlue: "#f0c098",
-      lightMagenta: "#ccc",
-      lightCyan: "#50e3c2"
-    },
-
     shell: "",
     shellArgs: ["--login"],
 
     bell: false
   },
-  plugins: []
+  plugins: ["hyper-website-theme"]
 };
