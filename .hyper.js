10fd144a046800ff2fcf9c1e0cf857b3b27a45af
not <mvllow@icloud.com>
2020-03-01 13:05:26 -0600

    use hyper-rose-pine

--- .hyper.js
+++ .hyper.js
@@ -5,12 +5,6 @@ module.exports = {
     fontSize: 12,
     fontFamily: "'SF Mono', SFMono-Regular, Menlo, Consolas",
 
-    cursorColor: "#fff",
-    foregroundColor: "#fff",
-    backgroundColor: "#000",
-    selectionColor: "rgba(255, 255, 255, 0.01)",
-    borderColor: "rgba(0, 0, 0, 0)",
-
     padding: "12px 30px 30px 30px",
 
     bell: false
