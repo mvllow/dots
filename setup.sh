0221e4317898672b28d0c91070cdd4243d4740b3
mellow <mvllow@icloud.com>
2020-06-09 15:54:28 -0500

    refactor setup.sh
    - easier to personalise
    - allow custom brewfile location
    - greatly improve comments/documentation
    - simplify arguments

--- setup.sh
+++ setup.sh
@@ -1,296 +1,281 @@
 #!/bin/sh
 
-app=~/.config/mvllow/dots
-url=https://github.com/mvllow/dots
-repo=https://github.com/mvllow/dots.git
+# --- START USER CONFIG ---
 
-cyan() {
-  echo "\033[0;36m$1\033[0m"
+# General
+app=~/.config/dots
+repo=https://github.com/mvllow/dots
+
+use_homebrew=true
+brewfile=$app/brewfile
+
+# These depend on an email address (usually the same as your Git[Hub/Lab/etc] account)
+# Uncomment the below line to hardcode your email or pass it via the [-e] flag
+# git_ssh_email=you@example.com
+maybe_config_git=true
+# Only tries if ~/.ssh/id_rsa doesn't already exists
+# If no email is provided, your computer's hostname will be used instead
+maybe_config_ssh=true
+
+# Copy preferences from repo for supported apps
+config_installed_apps=true
+
+# Changes macOS system preferences
+# Defaults can be found at https://github.com/mvllow/dots
+config_os_prefs=true
+
+# --- END USER CONFIG ---
+
+# echo text with blank line above
+echo_title() {
+  echo "\n$1"
 }
 
-gray() {
+# echo gray text
+echo_subtitle() {
   echo "\033[0;90m$1\033[0m"
 }
 
-item() {
-  echo "\033[0;90m> $1\033[0m"
-}
-
-put_header() {
-  clear
-  echo
-  echo "Minimalist developer 🌸"
-  echo
-}
-
-put_header
-
-while getopts u:e:rDh option; do
+# flags
+# a letter followed by `:` accepts an argument
+while getopts e:rDh option; do
   case "${option}" in
-    u) git_user=${OPTARG};;
-    e) git_email=${OPTARG};;
+    e) git_ssh_email=${OPTARG};;
     r) rm -rf $app;;
     D)
       rm -rf $app
       exit 2
       ;;
     h)
-      gray "Example usage:"
+      clear
+      echo_title "Minimalist developer 🌸"
       echo
-      echo "  sh $0 [-u USER] [-e EMAIL]"
+      echo_subtitle "Example usage:"
       echo
-      gray "Options:"
+      echo "  sh $0 [-e EMAIL]"
       echo
-      echo "  -u [user]           set user for global git config"
+      echo_subtitle "Options:"
+      echo
       echo "  -e [email]          set email for global git config and ssh keys"
       echo "  -r                  replace $app with remote"
       echo "  -D                  delete $app and exit"
       echo "  -h                  show this message"
       echo
-      gray "App settings:"
+      echo_subtitle "App settings:"
       echo
       echo "  $app"
       echo
-      gray "Documentation:"
+      echo_subtitle "Documentation:"
       echo
-      echo "  $url"
+      echo "  $repo"
       echo
       exit 2
       ;;
   esac
 done
 
-init() {
-  put_header
-
-  echo "Let's start with getting Homebrew"
-
-  if ! type brew &>/dev/null; then
-    item "https://brew.sh"
-
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  else
-    item "Already exists... skipping"
-  fi
-
-  echo
-
-  get_command_line_tools
-  get_repo
-
-  echo "Updating brew packages"
-  item "Using $app/brewfile"
-
-  brew upgrade &>/dev/null;
-  brew bundle --file="$app/brewfile" &>/dev/null;
-  brew cleanup &>/dev/null;
-
-  item "Cleaning up"
-  echo
-
-  config_git
-  config_ssh
-  config_apps
-  config_prefs
-  woohoo
-}
-
 get_command_line_tools() {
-  xcode-select --install &>/dev/null;
-
-  sleep 5
-
   if ! [ $(xcode-select --print-path) ]; then
-    put_header
+    xcode-select --install &>/dev/null;
 
-    echo "Waiting for command line tools to finish installing..."
-
+    sleep 3
+    
     get_command_line_tools
   fi
 }
 
-get_repo() {
-  echo "Fetching remote repo"
+clear
+echo_title "Minimalist developer 🌸"
 
+# if using macOS
+if [ $(uname) == "Darwin" ]; then
+  # if xcode command line tools don't exist
+  if ! [ $(xcode-select --print-path) ]; then
+    echo_title "Installing Command Line Tools"
+
+    get_command_line_tools
+  fi
+
+  # if $app directory doesn't exist
   if ! [ -e $app ]; then
-    item "Cloning to $app"
+    echo_title "Cloning remote repo"
+    echo_subtitle "$repo -> $app"
 
     mkdir -p $app
     git clone $repo $app &>/dev/null;
   else
-    item "$app exists... skipping"
+    echo_title "Using local repo"
+    echo_subtitle "$app"
   fi
 
-  echo
-}
+  if [ "$use_homebrew" = true ]; then
+    # if brew doesn't exist
+    if [ ! type brew &>/dev/null; then
+      echo_title "Installing Homebrew"
+      echo_subtitle "https://brew.sh"
 
-config_git() {
-  echo "Configuring global git"
-
-  if ! [ -f ~/.gitconfig ]; then
-    item "Setting general information"
-    if ! [[ -z ${git_user} ]]; then
-      git config --global user.name "$git_user"
+      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     fi
 
-    if ! [[ -z ${git_email} ]]; then
-      git config --global user.email "$git_email"
-    fi
+    echo_title "Installing Homebrew packages"
+    echo_subtitle "Using $brewfile"
 
-    if type nvim &>/dev/null; then
-      git config --global core.editor "nvim"
-    else
-      git config --global core.editor "vim"
-    fi
-
-    item "Adding aliases: CheckOut BRanch STatus"
-
-    git config --global alias.co checkout
-    git config --global alias.br branch
-    git config --global alias.st status
-  else
-    item "Already exists... skipping"
+    brew upgrade &>/dev/null;
+    echo
+    brew bundle --file="$brewfile"
+    echo
+    brew cleanup &>/dev/null;
   fi
 
-  echo
-}
+  if [ "$maybe_config_git" = true ]; then
+    # if $git_ssh_email isn't null
+    if ! [ -z ${git_ssh_email} ]; then
+      echo_title "Configuring global git"
+      echo_subtitle "Adding aliases:"
+      echo_subtitle "  co   -> checkout"
+      echo_subtitle "  br   -> branch"
+      echo_subtitle "  st   -> status"
+      echo_subtitle "  lola -> log --graph --decorate --pretty=oneline --abbrev-commit --all"
 
-config_ssh() {
-  echo "Configuring ssh keys"
+      git config --global user.email "$git_ssh_email"
+      git config --global alias.co checkout
+      git config --global alias.br branch
+      git config --global alias.st status
+      git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"
 
-  if ! [ -f ~/.ssh/id_rsa ]; then
-    if ! [[ -z ${git_email} ]]; then
-      item "Generating key with git email"
-      ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" -C $git_email
+      # if nvim exists
+      if type nvim &>/dev/null; then
+        git config --global core.editor "nvim"
+      else
+        git config --global core.editor "vim"
+      fi
     else
-      item "Generating key with computer hostname"
-      ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
+      echo_title "Skipping git config: no email found"
+      echo_subtitle "Try again with -e you@domain.com"
     fi
+  fi
+
+  if [ "$maybe_config_ssh" = true ]; then
+    # if ~/.ssh/id_rsa file doesn't exist
+    if ! [ -f ~/.ssh/id_rsa ]; then
+      # if $git_ssh_email isn't null
+      if ! [ -z ${git_ssh_email} ]; then
+        ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N "" -C $git_ssh_email
+      else
+        ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
+      fi
+    else
+      echo_title "Skipping ssh config: key exists"
+    fi
+
+    echo_title "Public key copied to clipboard"
+    echo_subtitle "~/.ssh/id_rsa.pub"
 
     pbcopy < ~/.ssh/id_rsa.pub
-
-    item "Public key copied to clipboard"
-    item "Saved to ~/.ssh/id_rsa.pub"
-  else
-    item "Already exists... skipping"
   fi
 
-  echo
-}
+  if [ "$config_installed_apps" = true ]; then
+    echo_title "Configuring installed apps"
 
-config_apps() {
-  echo "Updating app preferences"
+    if [ $(which zsh) ]; then
+      echo_subtitle "Copying Zsh settings"
+      cp -r $app/.zshrc ~/
+    fi
 
-  if [ $(which zsh) ]; then
-    item "Copying Zsh settings"
-    cp -r $app/.zshrc ~/
+    if [ $(which hyper) ]; then
+      echo_subtitle "Copying Hyper settings"
+      cp -r $app/.hyper.js ~/
+    fi
+
+    echo_subtitle "Copying Vim settings"
+    cp -r $app/.vimrc ~/
+
+    if [ $(which nvim) ]; then
+      echo_subtitle "Sharing Vim settings with NeoVim"
+
+      mkdir -p ~/.config/nvim
+      echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" > ~/.config/nvim/init.vim
+      echo "let &packpath=&runtimepath" >> ~/.config/nvim/init.vim
+      echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
+    fi
+
+    if [ $(which code-insiders) ]; then
+      echo_subtitle "Copying VSCode Insiders settings"
+
+      mkdir -p ~/Library/Application\ Support/Code\ -\ Insiders/User
+      cp $app/vscode/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
+
+      code-insiders --install-extension dbaeumer.vscode-eslint &>/dev/null;
+      code-insiders --install-extension esbenp.prettier-vscode &>/dev/null;
+      code-insiders --install-extension mvllow.rose-pine &>/dev/null;
+      code-insiders --install-extension vscodevim.vim &>/dev/null;
+    fi
+
+    if [ $(which subl) ]; then
+      echo_subtitle "Copying Sublime Text v3 settings"
+
+      mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+      cp -r $app/sublime/keymap.json ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+      cp -r $app/sublime/packages.json ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+      cp -r $app/sublime/settings.json ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+    fi
+
+    # Uncomment below to download sublime text v4
+    # sublime_v4_url=https://download.sublimetext.com/sublime_text_build_4074_mac.zip
+    # curl -sS $sublime_v4_url > sublime_text_4.zip
+    # unzip sublime_text_4.zip
+    # rm sublime_text_4.zip
+    # mv "Sublime Text.app" /Applications
+    # ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
   fi
 
-  if [ $(which hyper) ]; then
-    item "Copying Hyper settings"
-    cp -r $app/.hyper.js ~/
+  if [ "$config_os_prefs" = true ]; then
+    echo_title "Configuring macOS preferences"
+    echo_subtitle "Copying SF Mono to Font Book"
+
+    cp -r /System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts/. /Library/Fonts/
+
+    echo_subtitle "Modifying system defaults"
+    # Dock: enable autohide
+    defaults write com.apple.dock autohide -bool true
+    # Dock: hide recent apps
+    defaults write com.apple.dock show-recents -bool false
+    # Dock: show only active apps
+    defaults write com.apple.dock static-only -bool true
+    
+    # Keyboard: disable auto correct
+    defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+    # Keyboard: disable auto capitalise
+    defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+    # Keyboard: disable smart dashes
+    defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+    # Keyboard: disable smart quotes
+    defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+    # Keyboard: enable key repeat
+    defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+    # Keyboard: faster key repeat
+    defaults write NSGlobalDomain KeyRepeat -int 2
+    # Keyboard: shorter delay before key repeat
+    defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+    # Trackpad: enable tap to click (this user and login screen)
+    defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+    defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+    defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+    defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+    # Trackpad: increase tracking speed
+    defaults write -g com.apple.trackpad.scaling 3
+
+    # Finder: disable warning on file extension change
+    defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+    # Menubar: show battery percentage
+    defaults write com.apple.menuextra.battery ShowPercent -string "YES"
   fi
-
-  item "Copying Vim settings"
-  cp -r $app/.vimrc ~/
-
-  if [ $(which nvim) ]; then
-    item "Sharing Vim settings with NeoVim"
-    mkdir -p ~/.config/nvim
-    echo "set runtimepath^=~/.vim runtimepath+=~/.vim/after" > ~/.config/nvim/init.vim
-    echo "let &packpath=&runtimepath" >> ~/.config/nvim/init.vim
-    echo "source ~/.vimrc" >> ~/.config/nvim/init.vim
-  fi
-
-  if [ $(which code) ]; then
-    item "Copying VSCode settings"
-    mkdir -p ~/Library/Application\ Support/Code/User
-    cp $app/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-
-    code --install-extension dbaeumer.vscode-eslint &>/dev/null;
-    code --install-extension esbenp.prettier-vscode &>/dev/null;
-    code --install-extension mvllow.rose-pine &>/dev/null;
-    code --install-extension vscodevim.vim &>/dev/null;
-  fi
-
-  if [ $(which code-insiders) ]; then
-    item "Copying VSCode Insiders settings"
-    mkdir -p ~/Library/Application\ Support/Code\ -\ Insiders/User
-    cp $app/vscode/settings.json ~/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
-
-    code-insiders --install-extension dbaeumer.vscode-eslint &>/dev/null;
-    code-insiders --install-extension esbenp.prettier-vscode &>/dev/null;
-    code-insiders --install-extension mvllow.rose-pine &>/dev/null;
-    code-insiders --install-extension vscodevim.vim &>/dev/null;
-  fi
-
-  if [ $(which subl) ]; then
-    item "Copying Sublime Text settings"
-    mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-    cp -r $app/sublime/keymap.json ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-    cp -r $app/sublime/packages.json ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-    cp -r $app/sublime/settings.json ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-  fi
-
-  echo
-}
-
-config_prefs() {
-  echo "Configuring system"
-
-  item "Copying SF Mono to Font Book"
-  cp -r /System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts/. /Library/Fonts/
-
-  item "Modifying preferences"
-  # Dock: enable autohide
-  defaults write com.apple.dock autohide -bool true
-  # Dock: hide recent apps
-  defaults write com.apple.dock show-recents -bool false
-  # Dock: show only active apps
-  defaults write com.apple.dock static-only -bool true
-  
-  # Keyboard: disable auto correct
-  defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
-  # Keyboard: disable auto capitalise
-  defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
-  # Keyboard: disable smart dashes
-  defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-  # Keyboard: disable smart quotes
-  defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-  # Keyboard: enable key repeat
-  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-  # Keyboard: faster key repeat
-  defaults write NSGlobalDomain KeyRepeat -int 2
-  # Keyboard: shorter delay before key repeat
-  defaults write NSGlobalDomain InitialKeyRepeat -int 10
-
-  # Trackpad: enable tap to click (this user and login screen)
-  defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-  defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-  defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-  defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-  # Trackpad: increase tracking speed
-  defaults write -g com.apple.trackpad.scaling 3
-
-  # Finder: disable warning on file extension change
-  defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-  # Menubar: show battery percentage
-  defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
-  echo
-}
-
-woohoo() {
-  cyan "Done. Stay humble, stay hopeful."
-  echo
-}
-
-if [ $(uname) == "Darwin" ]; then
-  init
 else
-  put_header
+  echo_title "Unsupported OS"
+  echo_subtitle "$(uname)"
 
-  echo "Unsupported OS: $(uname)"
+  echo_title "Linux (testing on elementaryOS) support will come soon :)"
   exit 1
 fi
