55187f4274467534a180c0a0267a27841ecf7790
not <mvllow@icloud.com>
2021-04-03 22:34:29 -0500

    fix: poorly formatted ssh-keygen

--- setup.sh
+++ setup.sh
@@ -82,9 +82,9 @@ git config --global core.excludesfile ~/.gitignore
 git config --global user.name "$user"
 git config --global user.email "$email"
 
-if ! [ -f ~/.ssh/id_rsa ]; then
-    ssh-keygen -t rsa -b ed25519 -f ~/.ssh/id_rsa -q -N "" -C $email
-    pbcopy < ~/.ssh/id_rsa.pub
+if ! [ -f ~/.ssh/id_ed25519 ]; then
+    ssh-keygen -t ed25519 -C $email -f ~/.ssh/id_ed25519 -q -N ""
+    pbcopy < ~/.ssh/id_ed25519.pub
 fi
 
 if ! [ -f ~/.zshrc ]; then
