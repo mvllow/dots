02a1f9c9a046f2bc921116a9f1ddab734aa37667
not <mvllow@icloud.com>
2021-04-03 22:10:43 -0500

    fix: unknown brew location after install

--- setup.sh
+++ setup.sh
@@ -52,6 +52,7 @@ fi
 if ! [ $(which brew) ]; then
     echo "Installing brew"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    export PATH=/opt/homebrew/bin:$PATH
     echo
 fi
 
