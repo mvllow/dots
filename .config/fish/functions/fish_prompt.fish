a801663939f7f81a0e24e9cc9b5559d2c0da16a6
mvllow <mvllow@icloud.com>
2021-09-17 19:41:26 -0500

    chore: update git prompt colour

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt
     set -g fish_prompt_pwd_dir_length 0
-    printf '%s%s> ' (prompt_pwd) (set_color brblack; fish_git_prompt; set_color normal)
+    printf '%s%s> ' (prompt_pwd) (set_color yellow; fish_git_prompt; set_color normal)
 end
