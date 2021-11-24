86d83912b294953172267be2e9cbc8eb46d4df72
mvllow <mvllow@icloud.com>
2021-11-23 21:04:41 -0600

    feat: prefer jsonc over json

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -69,6 +69,7 @@ vim.opt.tabstop = 4
 
 vim.cmd('autocmd BufEnter * setlocal formatoptions-=o')
 vim.cmd('autocmd VimResized * tabdo wincmd =')
+vim.cmd('autocmd BufRead,BufNewFile *.json set filetype=jsonc')
 
 require('packer').startup(function(use)
 	use('wbthomason/packer.nvim')
