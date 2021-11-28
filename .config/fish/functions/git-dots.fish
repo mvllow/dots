e464235bf1f6b242c695f65a214b643d09e0f893
mvllow <mvllow@icloud.com>
2021-11-28 10:24:03 -0600

    chore: use existing `.git` alias

--- .config/fish/functions/git-dots.fish
+++ .config/fish/functions/git-dots.fish
@@ -1,9 +1,7 @@
 function git-dots -a directory -d "find untracked dotfiles"
-    alias _git "git --git-dir=$HOME/dots.git --work-tree=$HOME"
-
     if test -n "$directory"
-        _git ls-files --other "$directory/*"
+        .git ls-files --other "$directory/*"
     else
-        _git ls-files --other "$HOME/.config/*"
+        .git ls-files --other "$HOME/.config/*"
     end
 end
