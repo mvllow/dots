243c0ed300f5b4da76c5358fbee7cd648c341728
mvllow <mvllow@icloud.com>
2021-11-29 13:54:50 -0600

    chore: simplify toggle theme logic

--- .config/fish/functions/toggle-theme.fish
+++ .config/fish/functions/toggle-theme.fish
@@ -1,16 +1,11 @@
 function toggle-theme -d "toggle kitty theme (which will indirectly update fish)"
     use-terminal-colors
 
-    set current_theme (awk '$1=="include" {print $2}' "$HOME/.config/kitty/kitty.conf")
-    set new_theme "rose-pine.conf"
-
-    if [ "$current_theme" = "rose-pine.conf" ]
-        set new_theme "rose-pine-dawn.conf"
+    if [ "$THEME" = "Rosé Pine" ]
+        set -U THEME "Rosé Pine Dawn"
+    else
+        set -U THEME "Rosé Pine"
     end
 
-    # Set theme for active sessions. Requires `allow_remote_control yes`
-    kitty @ set-colors --all --configured "~/.config/kitty/$new_theme"
-
-    # Update config for persistence
-    sed -i '' -e "s/include.*/include $new_theme/" "$HOME/.config/kitty/kitty.conf"
+    kitty +kitten themes --reload-in=all $THEME
 end
