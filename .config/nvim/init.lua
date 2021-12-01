075a8cf0e072ee726e679e0aa7f58e6ad135c7f7
mvllow <mvllow@icloud.com>
2021-12-01 13:48:10 -0600

    chore: remove lsp handler

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -223,16 +223,6 @@ require('packer').startup(function(use)
 
 				vim.cmd('do User LspAttachBuffers')
 			end)
-
-			vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-				vim.lsp.diagnostic.on_publish_diagnostics,
-				{
-					signs = true,
-					underline = true,
-					update_in_insert = false,
-					virtual_text = false,
-				}
-			)
 		end,
 	})
 	use({
