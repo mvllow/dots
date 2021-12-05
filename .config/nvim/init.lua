a79252479eda1f8f40d4a4e2e067a730f7c03c10
mvllow <mvllow@icloud.com>
2021-12-05 16:41:44 -0600

    fix: impatient not found on first run

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@ local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.n
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	vim.fn.execute('!git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. install_path)
 else
-	require('impatient')
+	pcall(require, 'impatient')
 end
 
 local function map(mode, lhs, rhs, opts)
