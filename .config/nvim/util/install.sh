4df868d0655858fcb0eff8ad83773225b3b62b11
mvllow <mvllow@icloud.com>
2021-12-05 19:29:21 -0600

    chore: add tailwindcss server

--- .config/nvim/util/install.sh
+++ .config/nvim/util/install.sh
@@ -30,6 +30,6 @@ else
 	brew install neovim --HEAD
 fi
 
-npm install -g @fsouza/prettierd@latest typescript@latest typescript-language-server@latest vscode-langservers-extracted@latest svelte-language-server@latest
+npm install -g @fsouza/prettierd@latest typescript@latest typescript-language-server@latest vscode-langservers-extracted@latest @tailwindcss/language-server svelte-language-server@latest
 
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
