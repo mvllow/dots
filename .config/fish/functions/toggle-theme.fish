bffc06f924a790d6f7f8abd97b62fd8a7f31a5a8
mvllow <mvllow@icloud.com>
2021-12-12 11:19:53 -0600

    chore: toggle system theme via fish

--- .config/fish/functions/toggle-theme.fish
+++ .config/fish/functions/toggle-theme.fish
@@ -3,8 +3,10 @@ function toggle-theme -d "toggle kitty theme (which will indirectly update fish)
 
     if [ "$THEME" = "Rosé Pine" ]
         set -U THEME "Rosé Pine Dawn"
+        dark-mode off
     else
         set -U THEME "Rosé Pine"
+        dark-mode on
     end
 
     kitty +kitten themes --reload-in=all $THEME
