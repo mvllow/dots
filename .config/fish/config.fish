7c55a7ea20239c8855a7e5be27f708af0dc95da8
mvllow <mvllow@icloud.com>
2021-12-22 22:33:13 -0600

    chore(fish): match system theme on shell start

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,16 @@ if status is-interactive
         set -g fish_prompt_pwd_dir_length 0
         printf '%s%s> ' (prompt_pwd) (set_color yellow; fish_git_prompt; set_color normal)
     end
+
+    dark-mode status | read darkMode
+
+    if [ "$darkMode" = on ]
+        set -U THEME "Rosé Pine"
+    else
+        set -U THEME "Rosé Pine Dawn"
+    end
+
+    kitty +kitten themes --reload-in=all $THEME
 end
 
 # Set directory to buffer location for current session
