930ce5d547f4dadbb4ecd863413927e34c782c95
mvllow <mvllow@icloud.com>
2022-01-20 19:00:13 -0600

    chore: use sf mono

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -98,6 +98,7 @@ require('packer').startup(function(use)
 		'rose-pine/neovim',
 		as = 'rose-pine',
 		config = function()
+			vim.g.rose_pine_disable_italics = true
 			vim.cmd('colorscheme rose-pine')
 		end,
 	})
