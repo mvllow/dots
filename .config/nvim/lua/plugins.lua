ef171637f5efd14e1a76ab292424c0bad34eb4a2
mvllow <mvllow@icloud.com>
2022-02-09 12:54:12 -0600

    remove unnecessary nvim tree option

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -71,7 +71,6 @@ require('packer').startup(function(use)
 			}
 			vim.g.nvim_tree_quit_on_open = 1
 			vim.g.nvim_tree_show_icons = { folders = 1, files = 0 }
-			vim.g.nvim_tree_respect_buf_cwd = 1
 			require('nvim-tree').setup({
 				auto_close = true,
 				filters = { custom = { '.git' } },
