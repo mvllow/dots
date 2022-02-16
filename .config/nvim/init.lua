098908f8bd5015ef5f2b35741991887073af814d
mvllow <mvllow@icloud.com>
2022-02-16 12:15:26 -0600

    update packer bootstrap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -89,9 +89,9 @@ map('n', '<leader>h', ':TSHighlightCapturesUnderCursor<cr>', 'show highlight gro
 map('n', '<leader>pc', ':PackerCompile<cr>', 'compile plugins')
 map('n', '<leader>ps', ':PackerSync<cr>', 'sync plugins')
 
-local packer_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(packer_path)) > 0 then
-	vim.fn.execute('!git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. packer_path)
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	vim.fn.execute('!git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 require('packer').startup(function(use)
