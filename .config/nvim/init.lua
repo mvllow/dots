ad7d5398c8ee546c4c37e50492cee311426be1ec
mvllow <mvllow@icloud.com>
2022-03-02 14:28:21 -0500

    use autocmd api

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -219,7 +219,12 @@ require('packer').startup(function(use)
 				},
 				on_attach = function(client)
 					if client.resolved_capabilities.document_formatting then
-						vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]])
+						vim.api.nvim_create_autocmd('BufWritePre', {
+							pattern = '<buffer>',
+							callback = function()
+								vim.lsp.buf.formatting_sync()
+							end,
+						})
 					end
 				end,
 			})
