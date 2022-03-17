4b3d6094a4e9bc79d23f01834009a1e432a9d1b0
mvllow <mvllow@icloud.com>
2022-03-17 13:29:18 -0400

    add cargo to path and update abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ set -gx EDITOR nvim
 set -gx LS_COLORS true
 
 fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/.cargo/bin
 
 if status is-interactive
     set fish_greeting '🐟'
@@ -26,10 +27,14 @@ abbr --add .list "git --git-dir=\$HOME/dots.git --work-tree=\$HOME ls-files --ot
 
 # Manage cli configs
 abbr --add ,amfora "$EDITOR ~/.config/amfora/config.toml -c 'lcd %:p:h'"
+abbr --add ,dots "$EDITOR ~/.config/dots/setup.sh -c 'lcd %:p:h'"
 abbr --add ,fish "$EDITOR ~/.config/fish/config.fish -c 'lcd %:p:h'"
 abbr --add ,git "$EDITOR ~/.config/git/config -c 'lcd %:p:h'"
 abbr --add ,kitty "$EDITOR ~/.config/kitty/kitty.conf -c 'lcd %:p:h'"
 abbr --add ,lazygit "$EDITOR ~/.config/lazygit/config.yml -c 'lcd %:p:h'"
 abbr --add ,nvim "$EDITOR ~/.config/nvim/init.lua -c 'lcd %:p:h'"
+
+# Docker
+abbr --add da "docker run --rm -it -p 7655:22 alpine:latest"
 
 bind \e\[108\;9u toggle-theme # <super+l>
