7b9da6216a2233a26444b0fad98c6f9b567adecf
mvllow <mvllow@icloud.com>
2022-04-04 15:59:19 -0400

    allow passing only the repo name
    eg. `clone repo` will assume `clone <your git user.name>/repo`

--- .config/fish/functions/clone.fish
+++ .config/fish/functions/clone.fish
@@ -1,20 +1,34 @@
 # Clone remote repository and change to created directory
 #
-# @requires git
+# If only a repo is given, your git user.name is automatically used
+#
 # @usage
-# clone mvllow/fun-shell
+# clone dots <-- clones <your name>/dots
+# clone mvllow/pinecone
 # clone rose-pine/neovim rose-pine-neovim
 
-function clone --wrap 'git clone' -a repo
-    string match -rq '.*?\/(?<name>.*)' -- $repo
+function clone --wrap 'git clone' -a input
+    # Extract repo name out of "user/repo"
+    string match -rq '.*?\/(?<repo>.*)' -- $input
 
-    git clone git@github.com:$repo.git $argv[2..]
+    if test -n "$repo"
+        set url "$input"
+        set dest "$repo"
+    else
+        # Use git's user.name if user was not passed as input
+        # eg. if "repo" is passed, we assume "git_user_name/repo"
+        set url "$(git config --get user.name)/$input"
+        set dest "$input"
+    end
+
+    # Clone "user/repo" while passing any extra arguments
+    git clone git@github.com:$url.git $argv[2..]
 
     if test $status = 0
         if test $argv[2]
             cd $argv[2]
         else
-            cd $name
+            cd "$dest"
         end
     end
 end
