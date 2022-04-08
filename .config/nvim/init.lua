1a32c50e2b665090778ba0a4bc5680c23c0d0777
mvllow <mvllow@icloud.com>
2022-04-08 13:09:36 -0400

    increase column width

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -98,9 +98,7 @@ vim.opt.pumheight = 3
 --- Plugins
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	vim.fn.execute(
-		'!git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. install_path
-	)
+	vim.fn.execute('!git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. install_path)
 end
 
 require('packer').startup(function(use)
