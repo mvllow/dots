bbf94c4d1a8ae4c3df188bde024440f513268679
mvllow <mvllow@icloud.com>
2022-04-10 11:07:36 -0400

    remove deprecated treesitter option

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -176,7 +176,8 @@ require('packer').startup(function(use)
 		run = ':TSUpdate',
 		config = function()
 			require('nvim-treesitter.configs').setup({
-				ensure_installed = 'maintained',
+				ensure_installed = 'all',
+				ignore_install = { 'phpdoc' },
 				highlight = { enable = true },
 			})
 		end,
