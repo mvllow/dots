3d5bb75bfb8c571ebf1d8e77363cfa63fe3c4cb3
mvllow <mvllow@icloud.com>
2022-04-30 19:21:26 -0400

    set `XDG_{CACHE,DATA,STATE}_HOME` variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
+set -q XDG_CACHE_HOME; or set -xg XDG_CACHE_HOME $HOME/.cache
 set -q XDG_CONFIG_HOME; or set -xg XDG_CONFIG_HOME $HOME/.config
+set -q XDG_DATA_HOME; or set -xg XDG_DATA_HOME $HOME/.local/share
+set -q XDG_STATE_HOME; or set -xg XDG_STATE_HOME $HOME/.local/state
 set -gx EDITOR nvim
 
 fish_add_path /opt/homebrew/bin
