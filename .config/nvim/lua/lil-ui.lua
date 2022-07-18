7882af0a84fe5f5da804a14b7230f2f62d3ba599
mvllow <mvllow@icloud.com>
2022-07-17 23:11:45 -0400

    use un theme

--- .config/nvim/lua/lil-ui.lua
+++ .config/nvim/lua/lil-ui.lua
@@ -21,7 +21,8 @@ use({
 	as = 'rose-pine',
 	config = function()
 		require('rose-pine').setup({ disable_italics = true })
-		vim.cmd('colorscheme rose-pine')
+		-- vim.cmd('colorscheme rose-pine')
+		vim.cmd('colorscheme un')
 	end,
 })
 
