33307f5e22702f736a4d2a6b618a3d655d7d261b
mvllow <mvllow@icloud.com>
2022-08-06 13:23:48 -0400

    udpate dither script

--- .config/fish/functions/dither.fish
+++ .config/fish/functions/dither.fish
@@ -5,11 +5,10 @@
 # dither image.png
 # dither_bw image.png
 
-function dither -a image
-    convert "$image" -verbose -format GIF -interlace GIF -resize 640\> -ordered-dither 8x8 -set filename:f "%[t]_dithered" "%[filename:f].gif"
-end
-
-function dither_bw -a image
-    convert "$image" -verbose -format GIF -interlace GIF -resize 640\> -colorspace gray -colors 4 -ordered-dither 8x8 -set filename:f "%[t]_dithered" "%[filename:f].gif"
-
+function dither -a image -a bw
+    if test $bw
+        convert "$image" -verbose -format GIF -interlace GIF -resize 640\> -colorspace gray -colors 4 -ordered-dither 8x8 -set filename:f "%[t]_dithered" "%[filename:f].gif"
+    else
+        convert "$image" -verbose -format GIF -interlace GIF -resize 640\> -ordered-dither 8x8 -set filename:f "%[t]_dithered" "%[filename:f].gif"
+    end
 end
