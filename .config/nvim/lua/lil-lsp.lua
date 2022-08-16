0c8fe17e732bcd2a569b9e66b3af15a2516b0dd8
mvllow <mvllow@icloud.com>
2022-08-16 12:59:24 -0400

    use only null-ls for formatting

--- .config/nvim/lua/lil-lsp.lua
+++ .config/nvim/lua/lil-lsp.lua
@@ -79,20 +79,19 @@ use({
 			null_ls.builtins.formatting.stylua,
 		}
 
-		local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 		null_ls.setup({
 			sources = sources,
 			on_attach = function(client, bufnr)
 				if client.supports_method('textDocument/formatting') then
-					vim.api.nvim_clear_autocmds({
-						group = augroup,
-						buffer = bufnr,
-					})
 					vim.api.nvim_create_autocmd('BufWritePre', {
-						group = augroup,
 						buffer = bufnr,
 						callback = function()
-							vim.lsp.buf.format({ bufnr = bufnr })
+							vim.lsp.buf.format({
+								bufnr = bufnr,
+								filter = function(lsp_client)
+									return lsp_client.name == 'null-ls'
+								end,
+							})
 						end,
 					})
 				end
