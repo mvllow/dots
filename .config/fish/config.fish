63135785af1082f533c750ba5e15893a9cb16ae7
mvllow <mvllow@icloud.com>
2023-02-20 13:38:06 -0600

    Revert "add tmux"
    This reverts commit 416b31bd0c9340aad113efb32e2b76c16fece45e.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,18 +23,15 @@ set_theme system
 # Shortcuts to common config files
 # Loop through a list of paths, using the part before the "/" as the abbreviation name, prefixed with ","
 # @example `,fish` will open this file in $EDITOR
-set -l configs 'dots/setup.sh' 'fish/config.fish' git/config 'helix/config.toml' 'kitty/kitty.conf' 'lazygit/config.yml' 'nvim/init.lua' skhd/skhdrc 'tmux/tmux.conf' yabai/yabairc
+set -l configs 'dots/setup.sh' 'fish/config.fish' git/config 'helix/config.toml' 'kitty/kitty.conf' 'lazygit/config.yml' 'nvim/init.lua' skhd/skhdrc yabai/yabairc
 for config in $configs
     set app (string split "/" $config)[1]
-    abbr ",$app" "$EDITOR ~/.config/$config" # open config file in $EDITOR
+    abbr --add ",$app" "$EDITOR ~/.config/$config" # open config file in $EDITOR
 end
-
-abbr ta "tmux attach"
-abbr tn "tmux new -s (basename (pwd))"
 
 # Git helpers for our dotfiles bare repo
 # https://github.com/mvllow/dots
 set dotgit_args "--git-dir=\$HOME/dots.git --work-tree=\$HOME"
-abbr .git "git $dotgit_args"
-abbr .gitls "git $dotgit_args ls-files --others --no-empty-directory --exclude-standard \$XDG_CONFIG_HOME/*"
-abbr .lazygit "lazygit $dotgit_args"
+abbr --add .git "git $dotgit_args"
+abbr --add .gitls "git $dotgit_args ls-files --others --no-empty-directory --exclude-standard \$XDG_CONFIG_HOME/*"
+abbr --add .lazygit "lazygit $dotgit_args"
