ed83fbb6a167ef4858db8158125365508c1584bb
mvllow <mvllow@icloud.com>
2023-02-20 13:43:41 -0600

    nvim: bits and bobs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,3 @@
-require("user/package") -- loads lua/user/package.lua
-require("user/options") -- loads lua/user/options.lua
-require("user/keymaps") -- loads lua/user/keymaps.lua
-
-require("lazy").setup("plugins", {
-	install = { colorscheme = { "un" } },
-	change_detection = { notify = false },
-}) -- loads and merges each lua/plugins/*
+require("options") -- loads lua/options.lua
+require("keymaps") -- loads lua/keymaps.lua
+require("plugins") -- loads lua/plugins.lua
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -6,40 +6,44 @@ end
 
 vim.g.mapleader = " "
 
--- Improved searching.
+-- Improve search behaviour
 map("n", "<esc>", ":noh<cr>", { desc = "Clear search highlights" })
 map("n", "*", "*N", { desc = "Search under cursor" })
 map("v", "*", [[y/\V<c-r>=escape(@",'/\')<cr><cr>N]], { desc = "Search selection" })
 
--- Move through wrapped lines.
+-- Move through wrapped lines
 map({ "n", "v" }, "j", "gj")
 map({ "n", "v" }, "k", "gk")
 
--- Bubble lines.
-map("n", "<c-j>", ":m .+1<cr>==")
-map("n", "<c-k>", ":m .-2<cr>==")
-map("v", "<c-j>", ":m '>+1<cr>gv=gv")
-map("v", "<c-k>", ":m '<-2<cr>gv=gv")
-
--- Keep selection when indenting.
+-- Keep selection when indenting
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
--- Reindent entire file.
-map("n", "=", "mxggVG=`x")
-
--- Substitute current word.
+-- Substitute current word
 map("n", "S", ":%s/<c-r><c-w>//g<left><left>", { silent = false })
 
--- Goto helpers.
+-- Goto
 map("n", "go", "<c-o>", { desc = "Goto previous position" })
 map("n", "gO", "<c-i>", { desc = "Goto next position" })
 map("n", "gp", "<c-^>", { desc = "Goto previously focused buffer" })
 map({ "n", "v" }, "gm", "%", { desc = "Goto matching pair" })
 
--- Window controls.
+-- Window
 map("n", "<leader>wh", "<c-w>h", { desc = "Focus window to the left" })
 map("n", "<leader>wj", "<c-w>j", { desc = "Focus window below" })
 map("n", "<leader>wk", "<c-w>k", { desc = "Focus window above" })
 map("n", "<leader>wl", "<c-w>l", { desc = "Focus window to the right" })
 map("n", "<leader>wr", "<c-w>r", { desc = "Swap window positions" })
+map("n", "<leader>wK", "<c-w>t<c-w>K", { desc = "Switch horizontal split to vertical" })
+map("n", "<leader>wH", "<c-w>t<c-w>H", { desc = "Switch vertical split to horizontal" })
+
+local map_toggle = function(lhs, rhs, desc)
+	map("n", [[\]] .. lhs, rhs, { desc = desc })
+end
+
+map_toggle("b", '<cmd>lua vim.o.bg = vim.o.bg == "dark" and "light" or "dark"<cr>', "Toggle background")
+map_toggle("c", "<cmd>setlocal cursorline! cursorline?<cr>", "Toggle cursorline")
+map_toggle("l", "<cmd>setlocal list! list?<cr>", "Toggle list")
+map_toggle("n", "<cmd>setlocal number! number?<cr>", "Toggle number")
+map_toggle("s", "<cmd>setlocal spell! spell?<cr>", "Toggle spell")
+map_toggle("w", "<cmd>setlocal wrap! wrap?<cr>", "Toggle wrap")
