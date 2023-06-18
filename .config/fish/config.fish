73cfa277e3320793c66858cc8208d93cf3b2efb6
mvllow <mvllow@icloud.com>
2023-06-18 11:47:04 +0200

    remove lf, tmux, and unused fish abbrs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,14 +22,11 @@ set fish_greeting
 # Shortcuts to common config files
 # Loop through a list of paths, using the part before the "/" as the abbreviation name, prefixed with ","
 # @example `,fish` will open this file in $EDITOR
-set -l configs 'alacritty/alacritty.yml' 'dots/setup.sh' 'fish/config.fish' git/config 'helix/config.toml' 'kitty/kitty.conf' 'lazygit/config.yml' lf/lfrc 'nvim/init.lua' skhd/skhdrc 'tmux/tmux.conf' yabai/yabairc
+set -l configs 'dots/setup.sh' 'fish/config.fish' git/config 'kitty/kitty.conf' 'lazygit/config.yml' 'nvim/init.lua' skhd/skhdrc yabai/yabairc
 for config in $configs
     set app (string split "/" $config)[1]
     abbr ",$app" "$EDITOR ~/.config/$config" # open config file in $EDITOR
 end
-
-abbr ta "tmux attach"
-abbr tn "tmux new -s (basename (pwd))"
 
 # Git helpers for our dotfiles bare repo
 # https://github.com/mvllow/dots
