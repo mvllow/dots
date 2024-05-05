e6f9e0904b176f979ce4c8f6457ab02701a33df5
mvllow <mvllow@icloud.com>
2024-05-04 19:25:51 -0500

    feat(fish): overengineer toggle theme

--- .config/fish/functions/theme.fish
+++ .config/fish/functions/theme.fish
@@ -1,9 +1,19 @@
 function theme -a option
     set -q THEME
     set -U DARK_MODE $(dark-mode status)
+    set -U DARK_THEME rose-pine
+    set -U LIGHT_THEME rose-pine-dawn
+
+    function _set_ghostty_theme
+        sed -i "" -e \
+            "s/theme = .*/theme = $THEME/" \
+            "$HOME/.config/ghostty/config"
+    end
 
     function _set_kitty_theme
-        kitty @ set-colors --all --configured "$HOME/.config/kitty/themes/$THEME.conf"
+        if test "$TERM" = xterm-kitty
+            kitty @ set-colors --all --configured "$HOME/.config/kitty/themes/$THEME.conf"
+        end
         sed -i "" -e \
             "s/include themes\/.*\.conf/include themes\/$THEME/" \
             "$HOME/.config/kitty/kitty.conf"
@@ -11,16 +21,15 @@ function theme -a option
 
     function _toggle_theme
         if test "$DARK_MODE" = on
-            set -U THEME rose-pine-dawn
             set -U DARK_MODE off
+            set -U THEME $LIGHT_THEME
         else
-            set -U THEME rose-pine
             set -U DARK_MODE on
+            set -U THEME $DARK_THEME
         end
 
-        if test "$TERM" = xterm-kitty
-            _set_kitty_theme
-        end
+        _set_ghostty_theme
+        _set_kitty_theme
 
         # Toggle system theme last because it takes longer
         dark-mode
@@ -28,21 +37,29 @@ function theme -a option
 
     function _match_system_theme
         if test "$DARK_MODE" = on
-            set -U THEME rose-pine
+            set -U THEME $DARK_THEME
         else
-            set -U THEME rose-pine-dawn
+            set -U THEME $LIGHT_THEME
         end
 
-        if test "$TERM" = xterm-kitty
-            _set_kitty_theme
-        end
+        _set_ghostty_theme
+        _set_kitty_theme
     end
 
     function _help
+        set -l background (if test "$DARK_MODE" = on; echo dark; else; echo light; end)
+        set -l active_theme (if test "$DARK_MODE" = on; echo $DARK_THEME; else; echo $LIGHT_THEME; end)
+        set -l inactive_theme (if test "$DARK_MODE" = on; echo $LIGHT_THEME; else; echo $DARK_THEME; end)
+
+        echo
+        set_color --bold cyan
+        echo "* $active_theme"
+        set_color normal
+        echo "  $inactive_theme"
         echo
         echo "Usage: theme <option>"
-        echo "  toggle           Toggle theme"
-        echo "  match_system     Match system theme"
+        echo "  toggle           Toggle theme (→ $inactive_theme)"
+        echo "  match_system     Match system background ($background)"
         echo "  unset            Unset theme"
         echo
         echo "  -h, --help       Show this help message"
