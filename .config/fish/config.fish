6cad7ff3b21640a84fcb648dd51fe969024b3958
mvllow <mvllow@icloud.com>
2024-07-21 10:03:16 -0500

    feat(fish): check interactive status before setting abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,25 +13,27 @@ fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $GOPATH/bin
 
-set fish_greeting
+if status is-interactive
+    set fish_greeting
 
-# Open common configuration files
-# $ ,fish
-# > Opened ~/.config/fish in $EDITOR
-set -l configs "fish/config.fish" ghostty/config git/config "kitty/kitty.conf" "nvim/init.lua" skhd/skhdrc tmux/tmux.conf yabai/yabairc
-for config in $configs
-    set folder_name (string split "/" $config)[1]
-    set folder_path "~/.config/$config"
-    abbr ",$folder_name" "nvim $folder_path"
+    # Open common configuration files
+    # $ ,fish
+    # > Editing ~/.config/fish/config.fish in Neovim
+    set -l configs fish/config.fish ghostty/config git/config nvim/init.lua skhd/skhdrc tmux/tmux.conf yabai/yabairc
+    for config in $configs
+        set folder_name (string split "/" $config)[1]
+        set folder_path "$XDG_CONFIG_HOME/$config"
+        abbr ",$folder_name" "nvim $folder_path"
+    end
+
+    alias vim nvim
+    alias lg lazygit
+
+    abbr ta "tmux attach"
+    abbr tn "tmux new -s (basename (pwd))"
+
+    # Helpers to manage our dotfiles bare repo
+    # https://github.com/mvllow/dots
+    abbr .git "git --git-dir=\$HOME/dots.git --work-tree=\$HOME"
+    abbr .lg "lazygit --git-dir=\$HOME/dots.git --work-tree=\$HOME"
 end
-
-alias vim nvim
-alias lg lazygit
-
-abbr ta "tmux attach"
-abbr tn "tmux new -s (basename (pwd))"
-
-# Helpers to manage our dotfiles bare repo
-# https://github.com/mvllow/dots
-abbr .git "git --git-dir=\$HOME/dots.git --work-tree=\$HOME"
-abbr .lg "lazygit --git-dir=\$HOME/dots.git --work-tree=\$HOME"
