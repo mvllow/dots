be2ae2931074e92a41cfb33f09c6e7c4f8472fd4
mvllow <mvllow@icloud.com>
2024-12-02 10:56:35 -0600

    refactor(fish): drastically simplify theme function

--- .config/fish/functions/nvim.fish
+++ .config/fish/functions/nvim.fish
@@ -1,7 +1,10 @@
 function nvim -w nvim -d "Start Neovim with adaptive background"
-    if test "$DARK_MODE" = on
-        command nvim -c 'lcd %:p:h' -c 'set background=dark' $argv
-    else
-        command nvim -c 'lcd %:p:h' -c 'set background=light' $argv
+    set mode light
+    defaults read -g AppleInterfaceStyle >/dev/null 2>&1
+
+    if test $status -eq 0
+        set mode dark
     end
+
+    command nvim -c 'lcd %:p:h' -c "set background=$mode" $argv
 end
