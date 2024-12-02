c70ef844c291b64deefbbbcab48a5890d47cbf33
mvllow <mvllow@icloud.com>
2024-12-02 10:56:35 -0600

    refactor(fish): drastically simplify theme function

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-    theme match_system &
+    theme &
 
     set -gx __fish_git_prompt_showdirtystate 1
     printf '%s%s> ' (fish_prompt_pwd_dir_length=0 prompt_pwd) (set_color brblack; fish_git_prompt; set_color normal)
--- .config/fish/functions/theme.fish
+++ .config/fish/functions/theme.fish
@@ -1,87 +1,27 @@
-function theme -a option
-    set -q THEME
-    set -U DARK_MODE $(dark-mode status)
-    set -U DARK_THEME rose-pine
-    set -U LIGHT_THEME rose-pine-dawn
+function theme -a set_mode
+    set -l mode light
 
-    function _set_ghostty_theme
-        sed -i "" -e \
-            "s/theme = .*/theme = $THEME/" \
-            "$HOME/.config/ghostty/config"
-    end
+    if test -z $set_mode
+        defaults read -g AppleInterfaceStyle >/dev/null 2>&1
 
-    function _set_kitty_theme
-        if test "$TERM" = xterm-kitty
-            kitty @ set-colors --all --configured "$HOME/.config/kitty/themes/$THEME.conf"
+        if test $status -eq 0
+            set mode dark
         end
-        sed -i "" -e \
-            "s/include themes\/.*\.conf/include themes\/$THEME/" \
-            "$HOME/.config/kitty/kitty.conf"
-    end
-
-    function _toggle_theme
-        if test "$DARK_MODE" = on
-            set -U DARK_MODE off
-            set -U THEME $LIGHT_THEME
-        else
-            set -U DARK_MODE on
-            set -U THEME $DARK_THEME
+    else
+        switch $set_mode
+            case light
+                osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false" >/dev/null
+                set mode light
+            case dark
+                osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true" >/dev/null
+                set mode dark
         end
-
-        _set_ghostty_theme
-        _set_kitty_theme
-
-        # Toggle system theme last because it takes longer
-        dark-mode
     end
 
-    function _match_system_theme
-        if test "$DARK_MODE" = on
-            set -U THEME $DARK_THEME
-        else
-            set -U THEME $LIGHT_THEME
-        end
-
-        _set_ghostty_theme
-        _set_kitty_theme
-    end
-
-    function _help
-        set -l background (if test "$DARK_MODE" = on; echo dark; else; echo light; end)
-        set -l active_theme (if test "$DARK_MODE" = on; echo $DARK_THEME; else; echo $LIGHT_THEME; end)
-        set -l inactive_theme (if test "$DARK_MODE" = on; echo $LIGHT_THEME; else; echo $DARK_THEME; end)
-
-        echo
-        set_color --bold cyan
-        echo "* $active_theme"
-        set_color normal
-        echo "  $inactive_theme"
-        echo
-        echo "Usage: theme <option>"
-        echo "  toggle           Toggle theme (→ $inactive_theme)"
-        echo "  match_system     Match system background ($background)"
-        echo "  unset            Unset theme"
-        echo
-        echo "  -h, --help       Show this help message"
-        echo
-    end
-
-    if test -z "$option"
-        _help
-        return 1
-    end
-
-    switch "$option"
-        case toggle
-            _toggle_theme
-        case match_system
-            _match_system_theme
-        case unset
-            set -e THEME
-        case -h or --help
-            _help
-        case '*'
-            echo "Unknown option: $option"
-            return 1
+    switch $mode
+        case light
+            sed -i '' -e "s/^theme =.*/theme = rose-pine-dawn/" "$HOME/.config/ghostty/config"
+        case dark
+            sed -i '' -e "s/^theme =.*/theme = rose-pine/" "$HOME/.config/ghostty/config"
     end
 end
