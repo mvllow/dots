9c896cfc8981513b2ad64cbfb3b8ed83318e45aa
mvllow <mvllow@icloud.com>
2025-08-22 11:36:48 -0500

    fish: adapt for cross platform

--- .config/fish/functions/rm.fish
+++ .config/fish/functions/rm.fish
@@ -1,3 +1,19 @@
-function rm -w trash -d "Move to trash"
-    command trash -s $argv
+function rm -d "Move to trash"
+    set os_name (uname)
+
+    if test "$os_name" = Darwin
+        if not command -v trash >/dev/null
+            echo "unable to find command: 'trash'"
+            return
+        end
+        command trash -s $argv
+    else if test "$os_name" = Linux
+        if not command -v gio >/dev/null
+            echo "unable to find command: 'gio'"
+            return
+        end
+        command gio trash $argv
+    else
+        echo "unsupported os: '$os_name'"
+    end
 end
